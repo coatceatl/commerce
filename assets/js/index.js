74b9e5492ef9749c3b35b2f598f962bc2361d9eb
Оксана Ткаченко <oksana@otbook.local>
2019-01-03 15:10:26 +0200

    refactored code and added photos to markers

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 var map, pos, marker, circle, circleOptions, setCenter, users, title;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
+  myMap = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
     center: {lat: 32.0992549, lng: 34.8132114},
   });
@@ -9,21 +9,21 @@ function initMap() {
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      pos = {
+      myPosition = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
 
       marker = new google.maps.Marker({
-        position: pos,
-        map: map,
+        position: myPosition,
+        map: myMap,
       });
-      marker.setPosition(pos);
-      map.setCenter(pos);
+      marker.setPosition(myPosition);
+      myMap.setCenter(myPosition);
 
       var radius = 5;
       circleOptions = {
-        center: pos,
+        center: myPosition,
         fillColor: "#cceeff",
         fillOpacity: 0.5,
         strokeColor: "#99ddff",
@@ -34,7 +34,7 @@ function initMap() {
       }
 
       circle = new google.maps.Circle(circleOptions);
-      circle.setMap(map);
+      circle.setMap(myMap);
 
       var popupContent =
         '<div class="avatar-block">' +
@@ -49,34 +49,73 @@ function initMap() {
         '+7' +
         '</a>' +
         '</span>' +
-        '</div>'
+        '</div>';
 
-      users = [
-        ['User1',32.280054, 34.839627],
- //       ['User2',32.278959, 34.847759],
-   //     ['User3',32.272023, 34.840034]
+     var popupContent1 =
+        '<div class="avatar-block">' +
+        '<a href="#" class="map-link-avatar">' +
+        '<img class="map-avatar" src="assets/img/avatar-4.jpg">' +
+        '</a>' +
+        '<a href="#" class="map-link-avatar">' +
+        '<img class="map-avatar" src="assets/img/avatar-5.jpg">' +
+        '</a>' +
+        '<span class="map-avatar">' +
+        '<a href="#" class="map-link-avatar">' +
+        '+3' +
+        '</a>' +
+        '</span>' +
+        '</div>';
+
+     var popupContent2 =
+        '<div class="avatar-block">' +
+        '<a href="#" class="map-link-avatar">' +
+        '<img class="map-avatar" src="assets/img/avatar-1.jpg">' +
+        '</a>' +
+        '<a href="#" class="map-link-avatar">' +
+        '<img class="map-avatar" src="assets/img/avatar-6.jpg">' +
+        '</a>' +
+        '<span class="map-avatar">' +
+        '<a href="#" class="map-link-avatar">' +
+        '12' +
+        '</a>' +
+        '</span>' +
+        '</div>';
+
+      var icon = "https://maps.google.com/mapfiles/ms/icons/blue-dot.png";
+      var markers = [
+        {
+          coordinates: {lat: 32.280054, lng: 34.839627},
+          info: popupContent,
+        },
+        {
+          coordinates: {lat: 32.278959, lng: 34.847759},
+          info: popupContent1,
+        },
+        {
+          coordinates: {lat: 32.272023, lng: 34.840034},
+          info: popupContent2,
+        }
       ]
 
-      for (i=0; i<users.length; i++) {
-        var location = new google.maps.LatLng(users[i][1], users[i][2]);
-        var icon = "https://maps.google.com/mapfiles/ms/icons/blue-dot.png";
+      for (var i=0; i<markers.length; i++) {
+        addMarker(markers[i]);
+      }
+
+      function addMarker(properties) {
+        var markers = new google.maps.Marker({
+          position: properties.coordinates,
+          map: myMap,
+          icon: icon,
+        });
 
         var infoWindow = new google.maps.InfoWindow({
-          content: popupContent
+          content: properties.info
         });
 
-        var markers = new google.maps.Marker({
-          position: location,
-          map:map,
-          icon: icon,
-          title: users[i][0],
-        });
-
-        infoWindow.open(map, markers);
-
+        infoWindow.open(myMap, markers);
 
         markers.addListener('click', function() {
-          infoWindow.open(map, markers);
+          infoWindow.open(myMap, markers);
         });
       }
     });
